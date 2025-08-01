46f56aab867ed7a2ac180fef2fabe54407d85172
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2025-08-01 01:28:09 -0400

    Add RequireAuth component to protect routes and ensure user authentication

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,18 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 import Home from "./pages/Home.tsx";
+import RequireAuth from "./RequireAuth.tsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
-  { path: "/canchas", element: <Home /> },
+  {
+    path: "/canchas",
+    element: (
+      <RequireAuth>
+        <Home />
+      </RequireAuth>
+    ),
+  },
 ]);
 
 createRoot(document.getElementById("root")!).render(
